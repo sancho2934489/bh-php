5967bde7250832883813b65db54a2f94aa5a9ef0
sancho <sancho@windeath.ru>
2017-07-21 14:01:38 +0500

    BemHtmlConverter

--- src/BemHtmlConverter.php
+++ src/BemHtmlConverter.php
@@ -28,6 +28,8 @@ class BemHtmlConverter {
     public $blockElement = '';
     public $params = array();
 
+    private $matchCommand = '';
+
     function __construct($template/*,$bemPath,$projectPath=null*/)
     {
         $this->bemPath = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'BEM';
@@ -35,11 +37,22 @@ class BemHtmlConverter {
         $this->bh = new \BEM\BH();
     }
 
+    private function getFileJS($file) {
+        $file = $this->bemPath . DIRECTORY_SEPARATOR . $file;
+        $fileText = file_get_contents($file);
+        $json = preg_replace('/^[\s\S]*module.exports =|;[\s\S]*$/', '', $fileText);
+        return trim($json);
+    }
+
     private function getTextFile() {
         $file = $this->bemPath . DIRECTORY_SEPARATOR . self::BLOCKS . DIRECTORY_SEPARATOR . $this->template . DIRECTORY_SEPARATOR . $this->template . self::FILE_EXT;
         if ($textFile = file_get_contents($file)) {
             $textFile = trim($textFile);
-            var_dump($textFile);
+            while (preg_match('/include\(\'(.*?)\'\)/i',$textFile,$textFileMatches)) {
+                $filePath = $textFileMatches[1];
+                $json = $this->getFileJS($filePath);
+                $textFile = str_replace($textFileMatches[0],$json,$textFile);
+            }
             $textFile = preg_replace('/[\s\n\t]/','',$textFile);
             $this->textFile = $textFile;
         } else {
@@ -47,7 +60,7 @@ class BemHtmlConverter {
         }
     }
 
-    public function getElements() {
+    private function getElements() {
         $this->getTextFile();
         $blockElementString = '';
         if (preg_match('/block\([\'\"](.*?)[\'\"]\)/',$this->textFile, $block) && !preg_match('/block\([\'\"](.*?)[\'\"]\)\.elem\([\'\"](.*?)[\'\"]\)/',$this->textFile)) {
@@ -67,37 +80,36 @@ class BemHtmlConverter {
         $this->textFile = str_replace($blockElementString,'',$this->textFile);
     }
 
-    public function getContent() {
-        var_dump($this->textFile);
+    private function getContent() {
+        $this->getElements();
+        if (preg_match('/^\.match\(function\(\){return(.*?);\}\)\(/i',$this->textFile,$textFileMatches)) {
+            $attr = $textFileMatches[1];
+            if (preg_match('/^this\.ctx\.(.*?)$/i',$attr,$attrMatches)) {
+                $attr = $attrMatches[1];
+                $this->matchCommand = "\$check = \$ctx->ctx->".$attr.";";
+            }
+        }
+        //if
         if (preg_match('/content\(\)\((.*?)\)[\,\)]/i',$this->textFile,$textString)) {
-            var_dump($textString);
             $contentFunction = preg_replace('/^function\(\)\{return|\;\}$/','',$textString[1]);
-            var_dump($contentFunction);
             $this->textFile = str_replace($textString[0],'',$this->textFile);
-            var_dump($this->textFile);
             if (preg_match('/this\.ctx\.(.*?)\.map/i',$contentFunction,$textString)) {
-                var_dump($textString);
                 $this->varsName = $textString[1];
                 $contentFunction = preg_replace('/^'.$textString[0].'\(|\)$/','',$contentFunction);
-                var_dump($contentFunction);
-                print $contentFunction;
                 if (preg_match('/function\((.*?)\)\{return[\[\{](.*?)[,\]\}]\;\}/i',$contentFunction,$textString)) {
-                    var_dump($textString);
                     $this->varName = $textString[1];
                     $this->json = $textString[2];
                     if (!preg_match('/^\{|(\}\,|\})$/i',$this->json)) $this->json = '{'.$this->json.'}';
-                    var_dump($this->json);
                 }
             }
         }
     }
 
     public function setParams() {
+        $this->getContent();
         $items = '';
-        var_dump($this->params);
         foreach ($this->params as $param) {
             $item = $this->json;
-            var_dump($item);
             preg_match_all('/item\.([a-zA-Z\-0-9\_]*)/',$item,$varsitem);
             foreach ($varsitem[1] as $varitem) {
                 if (!isset($param[$varitem])) $param[$varitem] = '';
@@ -111,16 +123,13 @@ class BemHtmlConverter {
                     $replacement = (strlen($val) != 0)?$z[1]:''.$z[2];
                     $item = preg_replace('/'. $this->varName . '\.' . $key .'\?(.*?)\:\'\'(\)|\])/',$replacement,$item);
                 }
-                var_dump($this->varName . '.' . $key,"'$val'");
                 $item = str_replace($this->varName . '.' . $key,"'$val'",$item);
-                var_dump($item);
             }
             $items .= $item.',';
-            var_dump($items);
         }
-        var_dump('['.$items.']');
         $this->bh->match($this->blockElement,function ($ctx) use ($items) {
-            $ctx->content($this->bh->processBemjson('['.$items.']'));
+            eval($this->matchCommand);
+            if ($check || !isset($check)) $ctx->content($this->bh->processBemjson('['.$items.']'));
         });///////////////////////////////
 
     }
@@ -141,38 +150,30 @@ class BemHtmlConverter {
                     eval($command);
                 });
             }
-            var_dump($string);
             preg_match_all("/([a-zA-Z\-]*?)\(\)\(function\(\)\{return(.*?)}\)/i",$string,$z);
             for ($i=0;$i<count($z[1]);$i++) {
                 $action = $z[1][$i];
                 $response = $z[2][$i];
-                var_dump($response);
                 if (preg_match('/^\[(.*?)\]$/',$response,$responseMatchers)) {
                     $params = $responseMatchers[1];
                     $params = preg_split('/,/',$params);
-                    var_dump($params);
                     foreach ($params as $param) {
                         preg_match('/^\{(.*?)\:(.*?)\}$/i',$param,$paramMatchers);
                         $key = $paramMatchers[1];
                         $val = $paramMatchers[2];
                         if (preg_match('/^this\.ctx\.(.*?)$/i',$val,$valMatchers)) $val = $valMatchers[1];
                         $command = "\$ctx->".$action."(array('".$key."'=>\$ctx->ctx->".$val."));";
-                        var_dump($command);
                         $this->bh->match($block,function ($ctx) use ($command) {
                             eval($command);
                         });
                     }
                 } elseif (preg_match('/^\{(.*?)\}$/i',$response,$responseMatchers)) {
                     $param = $responseMatchers[1];
-                    var_dump($param);
                     preg_match('/^(.*?)\:(.*?)$/i',$param,$paramMatchers);
-                    var_dump($paramMatchers);
                     $key = $paramMatchers[1];
-                    var_dump($key);
                     $val = $paramMatchers[2];
                     if (preg_match('/^this\.ctx\.(.*?)$/i',$val,$valMatchers)) $val = $valMatchers[1];
                     $command = "\$ctx->".$action."(array('".$key."'=>\$ctx->ctx->".$val."));";
-                    var_dump($command);
                     $this->bh->match($block,function ($ctx) use ($command) {
                         eval($command);
                     });
@@ -189,12 +190,10 @@ class BemHtmlConverter {
             preg_match("/elem\(\'(.*?)\'\)\((.*?)\)$/",$string,$stringMatches);
             $elem = $this->block . '__' . $stringMatches[1];
             preg_match_all("/([a-zA-Z\-]*?)\(\)\((\'.*?\')\)/i",$string,$z);
-            var_dump($z);
             for ($i=0;$i<count($z[1]);$i++) {
                 $action = $z[1][$i];
                 $param = $z[2][$i];
                 $command = "\$ctx->".$action."(".$param.");";
-                var_dump($command);
                 $this->bh->match($elem,function ($ctx) use ($command) {
                     eval($command);
                 });
@@ -209,20 +208,16 @@ class BemHtmlConverter {
                     $params = preg_split('/,/',$params);
                     foreach ($params as $param) {
                         preg_match('/^\{(.*?)\:(.*?)\}$/i',$param,$paramMatchers);
-                        var_dump($paramMatchers);
                         $key = $paramMatchers[1];
                         $val = $paramMatchers[2];
                         if (preg_match('/^this\.ctx\.(.*?)$/i',$val,$valMatchers)) {
                             $val = $valMatchers[1];
                             $command = "\$ctx->".$action."(array('".$key."'=>\$ctx->ctx->".$val."));";
-                            var_dump($command);
                             $this->bh->match($elem,function ($ctx) use ($command) {
                                 eval($command);
                             });
                         } else {
                             $command = "\$ctx->" . $action . "(array('" . $key . "'=>" . $val . "));";
-                            var_dump($command);
-                            var_dump($elem);
                             $this->bh->match($elem,function ($ctx) use ($command) {
                                 eval($command);
                             });
@@ -230,15 +225,11 @@ class BemHtmlConverter {
                     }
                 } elseif (preg_match('/^\{(.*?)\}$/i',$response,$responseMatchers)) {
                     $param = $responseMatchers[1];
-                    var_dump($param);
                     preg_match('/^(.*?)\:(.*?)$/i',$param,$paramMatchers);
-                    var_dump($paramMatchers);
                     $key = $paramMatchers[1];
-                    var_dump($key);
                     $val = $paramMatchers[2];
                     if (preg_match('/^this\.ctx\.(.*?)$/i',$val,$valMatchers)) $val = $valMatchers[1];
                     $command = "\$ctx->".$action."(array('".$key."'=>\$ctx->ctx->".$val."));";
-                    var_dump($command);
                     $this->bh->match($elem,function ($ctx) use ($command) {
                         eval($command);
                     });
@@ -251,46 +242,28 @@ $start = microtime(true);
 
 $z = new BemHtmlConverter('post');
 $z->params = array(
-            array(
-                'title' => 'Больше не нужно заказывать пиццу домой Больше не нужно заказывать пиццу домой',
-                'text' => 'Вкусный, ароматный напиток с клюквой и Иван-чаем укрепит иммунитет и согреет в зимние холода Вкусный, ароматный напиток с клюквой и Иван-чаем укрепит иммунитет и согреет в зимние холода',
-                'label' => 'Кухни мира',
-                'image' => '/img/post-1.png',
-                'url' => '/'
-            ),
     array(
-        'title' => 'Больше не нужно заказывать пиццу домой Больше не нужно заказывать пиццу домой',
-        'text' => 'Вкусный, ароматный напиток с клюквой и Иван-чаем укрепит иммунитет и согреет в зимние холода Вкусный, ароматный напиток с клюквой и Иван-чаем укрепит иммунитет и согреет в зимние холода',
-        'label' => 'Кухни мира',
-        'image' => '/img/post-1.png',
-        'url' => '/'
+        'title' => 'title',
+        'text' => 'text',
+        'label' => 'label',
+        'image' => 'image',
+        'url' => 'url'
     ),
     array(
-        'title' => 'Больше не нужно заказывать пиццу домой Больше не нужно заказывать пиццу домой',
-        'text' => 'Вкусный, ароматный напиток с клюквой и Иван-чаем укрепит иммунитет и согреет в зимние холода Вкусный, ароматный напиток с клюквой и Иван-чаем укрепит иммунитет и согреет в зимние холода',
-        'label' => 'Кухни мира',
-        'image' => '/img/post-1.png',
-        'url' => '/'
+        'title' => 'title',
+        'text' => 'text',
+        'label' => 'label',
+        'image' => 'image',
+        'url' => 'url'
     ),
     array(
-        'title' => 'Больше не нужно заказывать пиццу домой Больше не нужно заказывать пиццу домой',
-        'text' => 'Вкусный, ароматный напиток с клюквой и Иван-чаем укрепит иммунитет и согреет в зимние холода Вкусный, ароматный напиток с клюквой и Иван-чаем укрепит иммунитет и согреет в зимние холода',
-        'label' => 'Кухни мира',
-        'image' => '/img/post-1.png',
-        'url' => '/'
-    ),
-    array(
-        'title' => 'Больше не нужно заказывать пиццу домой Больше не нужно заказывать пиццу домой',
-        'text' => 'Вкусный, ароматный напиток с клюквой и Иван-чаем укрепит иммунитет и согреет в зимние холода Вкусный, ароматный напиток с клюквой и Иван-чаем укрепит иммунитет и согреет в зимние холода',
-        'label' => 'Кухни мира',
-        'image' => '/img/post-1.png',
-        'url' => '/'
+        'title' => 'title',
+        'text' => 'text',
+        'label' => 'label',
+        'image' => 'image',
+        'url' => 'url'
     ),
 );
-$z->getElements();
-$z->getContent();
-
-//var_dump($z);
 
 $z->setParams();
 $z->updateBlock();
